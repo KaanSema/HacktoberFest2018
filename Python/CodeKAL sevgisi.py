868e1515dbfe250ac638ec99ebca5c4d81d5e71b
KaanSema <56893603+KaanSema@users.noreply.github.com>
2019-10-23 18:49:11 +0300

    Update CodeKAL sevgisi.py

--- Python/CodeKAL sevgisi.py
+++ Python/CodeKAL sevgisi.py
@@ -5,5 +5,8 @@ print("CodeKAL'ın bir parçası olmaktan çok mutluyum!")
 
 #GÖREV: Yukarıdaki komutların benzerin kullanarak İngilizce çıktılarını veriniz.
 
+print("I love CODEKAL")
+print("I am happy because I am a part of CODEKAL!")
+
 #YAZ: Aynı görevi java programlama dili ile de yaptınız. Bu iki dil arasında gördüğünüz farklar nedir?
 # başına "#" (yorum işareti) koyarak düşüncelerinizi kodun sonuna ekleyiniz.
