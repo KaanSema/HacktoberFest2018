03b3517de901c5a6a830d040708a904a0727d6c6
KaanSema <56893603+KaanSema@users.noreply.github.com>
2019-10-23 12:30:20 +0300

    Update Merhaba.py

--- Python/Merhaba.py
+++ Python/Merhaba.py
@@ -2,4 +2,6 @@
 #Aşağıdaki komut Merhaba, Dünya! çıktısı vermektedir. 
 print("Merhaba, Dünya")
 
+
 #GÖREV: Benzer bir komut yazarak bu sefer Merhaba, CodeKAL! çıktısı alın.
+print("Merhaba, CodeKAL!")
